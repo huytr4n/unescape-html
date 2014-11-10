b6e3d21c86ebfbf358b0a19e9a3ba6bc784ad175
Tran Quoc Huy <huy.tranquoc@asnet.com.vn>
2014-11-10 09:41:24 +0700

    change module to node exports

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /**
  * List out all special character on production db
  */
-var unescape = require('./unescape');
+var unescape = require('./index');
 var text = 'Omurtak Cad No 243/31 Tekirda&#x11f;, &#xc7;orlu 59850 Turkey';
 
 console.log(unescape(text));
